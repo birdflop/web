ad5e2ea06ff97cf98566e8a0f3bd07d0658e2dce
RiZeFlame <oliver.flynn025@gmail.com>
2022-02-24 17:46:41 -0800

    fix non english characters breaking preset

--- site/assets/js/AnimTABScript.js
+++ site/assets/js/AnimTABScript.js
@@ -428,12 +428,12 @@ function exportPreset() {
     savedColors[index] = value;
     colors[index] = value;
   });
-  let preset = btoa(colors + ":-:" + animationText.value + ":-:" + speed.value + ":-:" + animationTypes.value + ":-:" + compress(formats));
+  let preset = toBinary(colors + ":-:" + animationText.value + ":-:" + speed.value + ":-:" + animationTypes.value + ":-:" + compress(formats));
   return preset;
 }
 
 function importPreset(p) {
-  let preset = atob(p)
+  let preset = fromBinary(p)
   preset = preset.split(":-:")
   let colors = preset[0].split(",")
   animationText.value = preset[1]
@@ -490,6 +490,25 @@ function decompress(input, expectedValues) {
       values.push(value);
   }
   return values;
+}
+
+// convert a Unicode string to a string in which
+// each 16-bit unit occupies only one byte
+function toBinary(string) {
+  const codeUnits = new Uint16Array(string.length);
+  for (let i = 0; i < codeUnits.length; i++) {
+    codeUnits[i] = string.charCodeAt(i);
+  }
+  return btoa(String.fromCharCode(...new Uint8Array(codeUnits.buffer)));
+}
+
+function fromBinary(encoded) {
+  binary = atob(encoded)
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return String.fromCharCode(...new Uint16Array(bytes.buffer));
 }
 
 toggleColors(2);
--- site/assets/js/GradientScript.js
+++ site/assets/js/GradientScript.js
@@ -349,12 +349,12 @@ function exportPreset() {
     savedColors[index] = value;
     colors[index] = value;
   });
-  let preset = btoa(colors + ":-:" + nickName.value + ":-:" +   compress(formats));
+  let preset = toBinary(colors + ":-:" + nickName.value + ":-:" +   compress(formats));
   return preset;
 }
 
 function importPreset(p) {
-  let preset = atob(p)
+  let preset = fromBinary(p)
   preset = preset.split(":-:")
   let colors = preset[0].split(",")
   let nickname = preset[1]
@@ -411,6 +411,25 @@ function decompress(input, expectedValues) {
       values.push(value);
   }
   return values;
+}
+
+// convert a Unicode string to a string in which
+// each 16-bit unit occupies only one byte
+function toBinary(string) {
+  const codeUnits = new Uint16Array(string.length);
+  for (let i = 0; i < codeUnits.length; i++) {
+    codeUnits[i] = string.charCodeAt(i);
+  }
+  return btoa(String.fromCharCode(...new Uint8Array(codeUnits.buffer)));
+}
+
+function fromBinary(encoded) {
+  binary = atob(encoded)
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return String.fromCharCode(...new Uint16Array(bytes.buffer));
 }
 
 toggleColors(2);
