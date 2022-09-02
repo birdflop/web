b5ab1067c5f0d94e02f884297df11b93586478ff
AkiraDev <akira.devjs@gmail.com>
2022-09-02 15:45:59 -0700

    properly build

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ const path = require("path");
 let mainWindow
  
 // The absolute path of current this directory.
-const dataDir = path.resolve(`${process.cwd()}${path.sep}site`);
+const dataDir = path.resolve(`${__dirname}${path.sep}site`);
 
 // Absolute path of ./templates directory.
 const templateDir = path.resolve(`${dataDir}${path.sep}templates`);
 
 const renderTemplate = (mainWindow, template, data = {}) => {
     // Default base data which passed to the ejs template by default.
+
     ejse.data({
         path: `file://${templateDir}${path.sep}${template}`,
     });
-    mainWindow.loadURL(`file://${templateDir}${path.sep}${template}`);
+    mainWindow.loadURL(`${templateDir}${path.sep}${template}`);
 };
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
     })
-    
     renderTemplate(mainWindow, 'index.ejs')
 })
