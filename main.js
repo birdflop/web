fdf601cbcabd760f79d67380cda8474a80d9ff4f
saboooor <saboor547@gmail.com>
2022-09-02 17:29:35 -0600

    Use path.join instead of path.resolve and loadfile instead of loadurl and show the window when the page has loaded

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const {app, BrowserWindow} = require('electron')
-const ejse = require('ejs-electron')
+const { app, BrowserWindow } = require('electron');
+const ejse = require('ejs-electron');
 const path = require("path");
 
-let mainWindow
- 
+let mainWindow;
+
 // The absolute path of current this directory.
-const dataDir = path.resolve(`${__dirname}${path.sep}site`);
+const dataDir = path.join(__dirname, 'site');
 
 // Absolute path of ./templates directory.
-const templateDir = path.resolve(`${dataDir}${path.sep}templates`);
+const templateDir = path.join(dataDir, 'templates');
 
 const renderTemplate = (mainWindow, template, data = {}) => {
     // Default base data which passed to the ejs template by default.
-
     ejse.data({
-        path: `file://${templateDir}${path.sep}${template}`,
+        path: path.join(templateDir, template),
     });
-    mainWindow.loadURL(`${templateDir}${path.sep}${template}`);
+
+    // Load the template
+    mainWindow.loadFile(path.join(templateDir, template));
+
+    // Show the window after the template is loaded
+    mainWindow.webContents.on('ready-to-show', async () => mainWindow.show());
 };
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-    })
-    renderTemplate(mainWindow, 'index.ejs')
-})
+    mainWindow = new BrowserWindow({ show: false });
+    renderTemplate(mainWindow, 'index.ejs');
+});
