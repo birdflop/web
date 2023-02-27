39ee56c8cecee3f3d081b48b7ce59bb2736a5e54
saboooor <saboor547@gmail.com>
2023-02-26 19:21:15 -0700

    ok what about pure qwik

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
-import { imagetools } from "vite-imagetools";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { partytownVite } from "@builder.io/partytown/utils";
-import { join } from "path";
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [
-      qwikCity(),
-      qwikVite(),
-      tsconfigPaths(),
-      imagetools(),
-      partytownVite({ dest: join(__dirname, "dist", "~partytown") })
-],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
-        "Cache-Control": "public, max-age=600",
+        'Cache-Control': 'public, max-age=600',
       },
     },
   };
