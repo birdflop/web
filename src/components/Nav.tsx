31b145fd9f37e8a1ac855111cfc12f53c714a57d
saboooor <saboor547@gmail.com>
2023-03-11 23:12:23 -0700

    rid of presets

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -45,9 +45,6 @@ export default component$(() => {
                     <Link href="/AnimTab" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex items-center gap-4">
                       Animated TAB
                     </Link>
-                    <Link href="/Presets" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex items-center gap-4">
-                      Presets
-                    </Link>
                   </div>
                 </div>
               </button>
@@ -95,9 +92,6 @@ export default component$(() => {
           </Link>
           <Link href="/AnimTab" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex sm:hidden items-center gap-4">
             Animated TAB Gradients
-          </Link>
-          <Link href="/Presets" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex sm:hidden items-center gap-4">
-            Gradient Presets
           </Link>
           <Link href="/SparkProfile" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex sm:hidden items-center gap-4">
             Spark Profile Analysis
