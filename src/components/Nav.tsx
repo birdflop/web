0a0c20347db02e3957913fb721fc03b50463cdc9
saboooor <saboor547@gmail.com>
2023-03-12 18:04:00 -0600

    misctools dropdown

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -67,12 +67,25 @@ export default component$(() => {
                   </div>
                 </div>
               </button>
-              <Link href="/AnimTexture" class="transition duration-200 ease-in-out hidden bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md md:flex items-center gap-4">
-                Animated Textures
-              </Link>
-              <Link href="/MiscTools" class="transition duration-200 ease-in-out hidden bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md lg:flex items-center gap-4">
-                Misc Tools
-              </Link>
+              <button class="cursor-pointer transition duration-200 ease-in-out hidden lg:flex bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 group rounded-lg text-md items-center gap-4">
+                <div class="px-4 py-3 flex gap-4 items-center">
+                  Misc
+                  <ChevronDownIcon class="transform group-hover:-rotate-180 transition duration-300 ease-in-out" />
+                </div>
+                <div class="absolute top-0 left-0 z-10 hidden group-hover:flex pt-16">
+                  <div class="bg-black/50 rounded-xl px-3 py-4 flex flex-col space-y-2 font-medium whitespace-nowrap overflow-y-auto max-h-[calc(100svh-128px)]">
+                    <Link href="/AnimTexture" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex items-center gap-4">
+                      Animated Textures
+                    </Link>
+                    <Link href="/RamCalc" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex items-center gap-4">
+                      RAM Calculator
+                    </Link>
+                    <Link href="/ColorStrip" class="transition duration-200 ease-in-out bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md flex items-center gap-4">
+                      Color Code Stripper
+                    </Link>
+                  </div>
+                </div>
+              </button>
               <Link href="/Privacy" class="transition duration-200 ease-in-out hidden bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl border-2 border-gray-900 hover:border-gray-700 px-4 py-2 rounded-lg text-md lg:flex items-center gap-4">
                 Privacy Policy
               </Link>
