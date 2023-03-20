87ab53d3d4e709b311ae16267eb5c39a6e66ee90
saboooor <saboor547@gmail.com>
2023-03-19 20:25:14 -0600

    fuck canvas but im keeping the api

--- src/routes/api/gradient/index.tsx
+++ src/routes/api/gradient/index.tsx
@@ -1,16 +1,55 @@
 import { generateOutput } from '~/components/util/RGBUtils';
-import { createPreview } from '~/components/util/HexCanvas';
-import { server$ } from '@builder.io/qwik-city';
-
-const createPreviewonServer = server$(async (text, colors, prefix, bold, italic, underline, strikethrough) => await createPreview(text, colors?.split(','), prefix, bold, italic, underline, strikethrough));
 
 export const onGet: any = async ({ json, query }: any) => {
-  const { text, colors, format, formatchar, prefix = '', bold = false, italic = false, underline = false, strikethrough = false } = Object.fromEntries(query);
+  const { text, colors, format, formatchar, prefix = '', bold = false, italic = false, underline = false, strikethrough = false, silent = false } = Object.fromEntries(query);
 
-  const preview = await createPreviewonServer(text, colors, prefix, bold, italic, underline, strikethrough);
+  const options = silent ? {} : {
+    options: {
+      text: {
+        url: 'text=exampleText',
+        description: 'The text to use for the gradient.',
+      },
+      colors: {
+        url: 'colors=#rrggbb,#rrggbb,#rrggbb...',
+        description: 'The colors to use for the gradient. Must be in hex format.',
+      },
+      format: {
+        url: 'format=&#$1$2$3$4$5$6$f$c',
+        description: 'The format to use for the color codes. $1 = #(r)rggbb, $2 = #r(r)ggbb, $3 = #rr(g)gbb, $4 = #rrg(g)bb, $5 = #rrgg(b)b, $6 = #rrggb(b), $f = format tags, $c = the character',
+      },
+      formatchar: {
+        url: 'formatchar=&',
+        description: 'The character to use for the format tags. (such as &l, &o, &n, &m)',
+      },
+      prefix: {
+        url: 'prefix=/nick',
+        description: 'The prefix to use for the text. Usually used for commands and stuff.',
+      },
+      bold: {
+        url: 'bold=true',
+        description: 'Whether or not to bold the text.',
+      },
+      italic: {
+        url: 'italic=true',
+        description: 'Whether or not to italicize the text.',
+      },
+      underline: {
+        url: 'underline=true',
+        description: 'Whether or not to underline the text.',
+      },
+      strikethrough: {
+        url: 'strikethrough=true',
+        description: 'Whether or not to strikethrough the text.',
+      },
+      silent: {
+        url: 'silent=true',
+        description: 'Set this to true to hide these options.',
+      },
+    },
+  };
+
   throw json(200, {
     output: generateOutput(text, colors?.split(','), format, formatchar, prefix, bold, italic, underline, strikethrough),
-    preview,
-    notice: 'This API is unfinished.',
+    ...options,
   });
 };
