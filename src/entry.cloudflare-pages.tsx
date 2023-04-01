794d2cea7d7f3fd8b260dda34e67da6cc0dc1261
saboooor <saboor547@gmail.com>
2023-04-01 14:33:08 -0600

    update pkgs and cf pages

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -4,7 +4,7 @@
  * It's the  entry point for cloudflare-pages when building for production.
  *
  * Learn more about the cloudflare integration here:
- * - https://qwik.builder.io/integrations/deployments/cloudflare-pages/
+ * - https://qwik.builder.io/deployments/cloudflare-pages/
  *
  */
 import {
@@ -12,12 +12,13 @@ import {
   type PlatformCloudflarePages,
 } from '@builder.io/qwik-city/middleware/cloudflare-pages';
 import qwikCityPlan from '@qwik-city-plan';
+import { manifest } from '@qwik-client-manifest';
 import render from './entry.ssr';
 
 declare global {
   interface QwikCityPlatform extends PlatformCloudflarePages {}
 }
 
-const onRequest = createQwikCity({ render, qwikCityPlan });
+const onRequest = createQwikCity({ render, qwikCityPlan, manifest });
 
 export { onRequest };
