4ce8085d67ecb190804d139fe99e3641cb5c7c32
saboooor <saboor547@gmail.com>
2023-12-17 18:17:42 -0700

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,9 +31,7 @@ pub fn run() {
       }
     })
     .plugin(tauri_plugin_notification::init())
-    .plugin(tauri_plugin_window::init())
     .plugin(tauri_plugin_window_state::Builder::default().build())
-    .plugin(tauri_plugin_app::init())
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
