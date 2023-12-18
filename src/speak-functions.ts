69a095fff592e190afec29617d6be8b64afbd4c1
AkiraDev <akira.devjs@gmail.com>
2023-12-17 16:13:43 -0800

    i give up

--- src/speak-functions.ts
+++ src/speak-functions.ts
@@ -11,9 +11,9 @@ const translationData = import.meta.glob<Translation>('/i18n/**/*.json');
  * Using server$, translation data is always accessed on the server
  */
 const loadTranslation$: LoadTranslationFn = server$(async (lang: string, asset: string) =>
-  await translationData[`/i18n/${lang}/${asset}.json`]?.()
+  await translationData[`/i18n/${lang}/${asset}.json`]?.(),
 );
 
 export const translationFn: TranslationFn = {
-  loadTranslation$: loadTranslation$
+  loadTranslation$: loadTranslation$,
 };
