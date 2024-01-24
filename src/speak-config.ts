36df5c55fc8179538e5e8e76399b22d18e4a3d13
saboooor <saboor547@gmail.com>
2024-01-23 22:38:48 -0700

    add korean

--- src/speak-config.ts
+++ src/speak-config.ts
@@ -3,10 +3,12 @@ export const languages = {
   'da-DK': 'Dansk',
   'en-US': 'English',
   'es-ES': 'Español',
+  'ko-KR': '한국어',
   'nl-NL': 'Nederlands',
   'pl-PL': 'Polski',
   'pt-PT': 'Português',
   'ru-RU': 'Русский',
+  'zh-CN': '中文',
 };
 
 export const config: SpeakConfig = {
