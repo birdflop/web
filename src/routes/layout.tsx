9aa20c737ad5666104e7373f3ff1e7938fff229e
AkiraDev <akira.devjs@gmail.com>
2024-02-12 23:40:16 -0800

    work? pls and thx

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -46,25 +46,26 @@ export default component$(() => {
   </>;
 });
 
-export const onRequest: RequestHandler = ({ request, locale }) => {
-  const cookie = request.headers?.get('cookie');
-  const acceptLanguage = request.headers?.get('accept-language');
+// export const onRequest: RequestHandler = ({ request, locale }) => {
+//   const cookie = request.headers?.get('cookie');
+//   const acceptLanguage = request.headers?.get('accept-language');
 
-  let lang: string | null = null;
-  // Try whether the language is stored in a cookie
-  if (cookie) {
-    const result = new RegExp('(?:^|; )' + encodeURIComponent('locale') + '=([^;]*)').exec(cookie);
-    if (result) {
-      lang = JSON.parse(result[1])['lang'];
-    }
-  }
-  // Try to use user language
-  if (!lang) {
-    if (acceptLanguage) {
-      lang = acceptLanguage.split(';')[0]?.split(',')[0];
-    }
-  }
+//   let lang: string | null = null;
+//   // Try whether the language is stored in a cookie
+//   if (cookie) {
+//     const result = new RegExp('(?:^|; )' + encodeURIComponent('locale') + '=([^;]*)').exec(cookie);
+//     if (result) {
+//       lang = JSON.parse(result[1])['lang'];
+//     }
+//   }
+//   // Try to use user language
+//   if (!lang) {
+//     if (acceptLanguage) {
+//       lang = acceptLanguage.split(';')[0]?.split(',')[0];
+//     }
+//   }
 
-  // Set Qwik locale
-  locale(lang || config.defaultLocale.lang);
-};
+//   // Set Qwik locale
+//   console.log(lang);
+//   locale(lang || config.defaultLocale.lang);
+// };
