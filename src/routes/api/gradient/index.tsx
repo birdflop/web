1e7ae437de112256c45d1febe19d903c78cc22b6
saboooooor <saboor547@gmail.com>
2024-06-05 04:48:39 -0600

    temp fix api

--- src/routes/api/gradient/index.tsx
+++ src/routes/api/gradient/index.tsx
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@builder.io/qwik-city';
 import { generateOutput } from '~/components/util/RGBUtils';
 
 export const onGet: RequestHandler = async ({ json, query }) => {
-  const { text, colors, format, formatchar, prefix = '', bold = 'false', italic = 'false', underline = 'false', strikethrough = 'false', silent = 'false' } = Object.fromEntries(query);
+  const { text, colors, format, prefix = '', bold = 'false', italic = 'false', underline = 'false', strikethrough = 'false', silent = 'false' } = Object.fromEntries(query);
 
   const options = silent ? {} : {
     options: {
@@ -17,10 +17,6 @@ export const onGet: RequestHandler = async ({ json, query }) => {
       format: {
         url: 'format=&#$1$2$3$4$5$6$f$c',
         description: 'The format to use for the color codes. $1 = #(r)rggbb, $2 = #r(r)ggbb, $3 = #rr(g)gbb, $4 = #rrg(g)bb, $5 = #rrgg(b)b, $6 = #rrggb(b), $f = format tags, $c = the character',
-      },
-      formatchar: {
-        url: 'formatchar=&',
-        description: 'The character to use for the format tags. (such as &l, &o, &n, &m)',
       },
       prefix: {
         url: 'prefix=/nick',
@@ -50,7 +46,7 @@ export const onGet: RequestHandler = async ({ json, query }) => {
   };
 
   throw json(200, {
-    output: generateOutput(text, colors?.split(','), format, formatchar, prefix, bold == 'true', italic == 'true', underline == 'true', strikethrough == 'true'),
+    output: generateOutput(text, colors?.split(','), { color: format, char: '&' }, `${prefix}$t`, true, bold == 'true', italic == 'true', underline == 'true', strikethrough == 'true'),
     ...options,
   });
 };
