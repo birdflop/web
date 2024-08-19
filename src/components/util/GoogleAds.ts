6f001766c29228f7496126bd782e08e4d1f2e490
Purpur <43528123+Pemigrade@users.noreply.github.com>
2024-08-19 17:05:23 -0500

    declare adsbygoogle

--- src/components/util/GoogleAds.ts
+++ src/components/util/GoogleAds.ts
@@ -1,15 +1,16 @@
 // src/utils/GoogleAds.ts
+let adsbygoogle: any;
+
 export function unloadGoogleAds() {
-    const ads = document.querySelectorAll('.adsbygoogle');
-    ads.forEach(ad => ad.remove());
-  
-    if (typeof adsbygoogle !== 'undefined' && adsbygoogle.length) {
-      adsbygoogle = [];
-    }
-  
-    const adsScript = document.querySelector('script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"]');
-    if (adsScript) {
-      adsScript.remove();
-    }
+  const ads = document.querySelectorAll('.adsbygoogle');
+  ads.forEach(ad => ad.remove());
+
+  if (typeof adsbygoogle !== 'undefined' && adsbygoogle.length) {
+    adsbygoogle = [];
   }
-  
+
+  const adsScript = document.querySelector('script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"]');
+  if (adsScript) {
+    adsScript.remove();
+  }
+}
