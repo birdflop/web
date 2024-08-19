2bd7675185656eb8de693fb449d363a2f6b256a3
Purpur <43528123+Pemigrade@users.noreply.github.com>
2024-08-19 17:33:36 -0500

    simplify / don't try to remove unused elts

--- src/components/util/GoogleAds.ts
+++ src/components/util/GoogleAds.ts
@@ -1,18 +1,7 @@
 // src/components/util/GoogleAds.ts
-declare let adsbygoogle: any;
-
 export function unloadGoogleAds() {
   if (typeof document !== 'undefined') {
     const ads = document.querySelectorAll('.adsbygoogle');
     ads.forEach(ad => ad.remove());
-
-    if (typeof adsbygoogle !== 'undefined' && adsbygoogle.length) {
-      adsbygoogle = [];
-    }
-
-    const adsScript = document.querySelector('script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"]');
-    if (adsScript) {
-      adsScript.remove();
-    }
   }
 }
