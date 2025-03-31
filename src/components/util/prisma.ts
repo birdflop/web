2f9faa3cf0ba67745a7ebc417a0b4b663ba44c3f
saboooor <saboor547@gmail.com>
2025-03-31 07:08:29 -0600

    let's see if this works

--- src/components/util/prisma.ts
+++ src/components/util/prisma.ts
@@ -1,14 +1,18 @@
+import { server$ } from '@builder.io/qwik-city';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-}).$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma
+  || await server$(function () {
+    return new PrismaClient({
+      datasources: {
+        db: {
+          url: this.env.get('DATABASE_URL'),
+        },
+      },
+    }).$extends(withAccelerate());
+  })();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
