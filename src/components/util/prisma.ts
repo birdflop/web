48d10dc5471b55130d1dd2e8e9c516dfbd42bd92
saboooor <saboor547@gmail.com>
2025-03-31 06:42:26 -0600

    fix?

--- src/components/util/prisma.ts
+++ src/components/util/prisma.ts
@@ -3,6 +3,12 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+}).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
