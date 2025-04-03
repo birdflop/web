137d1987d8fc4135981ec3950a787437806e2414
saboooor <saboor547@gmail.com>
2025-04-03 14:08:05 -0600

    delete v1 api

--- src/routes/api/index.tsx
+++ src/routes/api/index.tsx
@@ -3,10 +3,6 @@ import type { RequestHandler } from '@builder.io/qwik-city';
 export const onGet: RequestHandler = async ({ json }) => {
   throw json(200, {
     endpoints: {
-      '/api/gradient': {
-        GET: 'Generate a gradient.',
-        WARNING: '/api/gradient is deprecated. Please use /api/v2/rgb instead.',
-      },
       '/api/v2': {
         GET: 'View the v2 API endpoints.',
       },
