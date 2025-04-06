a2a0014d8ce984e574e80818bfbd2e96d206140c
saboooor <saboor547@gmail.com>
2025-04-06 15:54:58 -0600

    yh

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -95,7 +95,7 @@ export default component$(() => {
     <Nav />
     <Background id="bg" class={{
       'fixed scale-120 bottom-0 overflow-hidden -z-10 w-full h-full object-cover brightness-50': true,
-      'blur-xl opacity-10': loc.url.pathname != '/',
+      'transition-all duration-1000 blur-xl opacity-10 scale-150': loc.url.pathname != '/',
     }}/>
     <Slot />
     <div class={{
