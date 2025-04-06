810ac60b5860227234972f6b04a7a5e573a8f493
kakduman <pemigrade@gmail.com>
2025-04-06 17:51:11 -0400

    change to rates (because high=good)

--- src/data/historicPrices.ts
+++ src/data/historicPrices.ts
@@ -3,16 +3,16 @@ export const historicPrices = {
   labels: ['Q3 2023', 'Q4 2023', 'Q1 2024', 'Q2 2024', 'Q3 2024', 'Q4 2024', 'Q1 2025'],
   datasets: [
     {
-      label: 'US Prices ($/GB RAM)',
-      data: [2.33, 1.95, 1.95, 1.95, 1.89, 1.89, 1.99],
+      label: 'US Reimbursements (%)',
+      data: [22.2, 35.0, 35.0, 35.0, 37.0, 37.0, 33.7],
       borderColor: '#55BBFF',
       backgroundColor: 'transparent',
       fill: false,
       tension: 0.1,
     },
     {
-      label: 'EU Prices ($/GB RAM)',
-      data: [2.00, 1.88, 1.58, 1.58, 1.48, 1.46, 1.44],
+      label: 'EU Reimbursements (%)',
+      data: [0.0, 6.0, 21.0, 21.0, 26.0, 27.0, 28.0],
       borderColor: '#55FFBB',
       backgroundColor: 'transparent',
       fill: false,
