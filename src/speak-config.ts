04c91da80b270c80f2ddcb7413c0ea09fb09d9e1
saboooor <saboor547@gmail.com>
2025-04-08 16:27:53 -0600

    Update all existing lang files

--- src/speak-config.ts
+++ src/speak-config.ts
@@ -1,11 +1,9 @@
 import type { SpeakConfig } from 'qwik-speak';
 export const languages = {
-  'da-DK': 'Dansk',
   'en-US': 'English',
   'es-ES': 'Español',
   'ko-KR': '한국어',
   'nl-NL': 'Nederlands',
-  'pl-PL': 'Polski',
   'pt-PT': 'Português',
   'ru-RU': 'Русский',
   'zh-CN': '中文',
