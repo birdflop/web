a6edc31cf24866b13a758dac9e580d735bdec391
saboooor <saboor547@gmail.com>
2025-04-16 19:07:17 -0600

    uhh

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,7 @@ module.exports = {
   "reject": [
     "eslint", // new major version which idk how to fix yet with qwik
     "@types/eslint",
-    "vite" // building broken on v6
+    "vite", // building broken on v6
+    "prisma" // building broken on v6.6.0
   ],
 }
