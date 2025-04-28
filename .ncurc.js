f8bb46e8396210bb58e52aaefa94d482f2ccd4c0
saboooor <saboor547@gmail.com>
2025-04-28 02:46:10 -0600

    update pkgs except prisma

--- .ncurc.js
+++ .ncurc.js
@@ -3,6 +3,5 @@ module.exports = {
     "eslint", // new major version which idk how to fix yet with qwik
     "@types/eslint",
     "vite", // building broken on v6
-    "@prisma/client" // building broken on v6.6.0
   ],
 }
