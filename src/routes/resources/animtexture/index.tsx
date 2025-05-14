223097967ab31a57417acf343954db686c1f541b
saboooor <saboor547@gmail.com>
2025-05-14 01:55:29 -0600

    use gifuct maybe

--- src/routes/resources/animtexture/index.tsx
+++ src/routes/resources/animtexture/index.tsx
@@ -1,151 +1,172 @@
-import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik';
+import { component$, isBrowser, useStore, useTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import { Toggle } from '@luminescent/ui-qwik';
 import { inlineTranslate } from 'qwik-speak';
+
+import { parseGIF, decompressFrames } from 'gifuct-js';
+import { Download } from 'lucide-icons-qwik';
+
+export async function base64ToFile(dataURL: string) {
+  const arr = dataURL.split(',');
+  const match = arr[0].match(/:(.*?);/);
+  const mime = match ? match[1] : '';
+  const result = await fetch(dataURL);
+  return {
+    mime,
+    buffer: await result.arrayBuffer(),
+  };
+};
 
 export default component$(() => {
   const t = inlineTranslate();
 
   const animtextureStore = useStore({
-    frames: [] as any[],
-    textureName: '',
-    cumulative: false,
+    frames: [] as { img: HTMLImageElement, delay: number }[],
+    textureName: 'animtexture',
   }, { deep: true });
 
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(() => {
-    if (document.getElementsByName('gifframes')[0]) return;
-    const script = document.createElement('script');
-    script.src = '/scripts/gif-frames.js';
-    script.defer = true;
-    script.setAttribute('name', 'gifframes');
-    document.head.appendChild(script);
+  useTask$(({ track }) => {
+    track(() => animtextureStore.frames);
+    console.log('rerendering');
+    if (!isBrowser) return;
+
+    const canvas = document.getElementById('c') as HTMLCanvasElement;
+    if (!canvas) return;
+    const ctx = canvas.getContext('2d')!;
+
+    let maxWidth = 0;
+    let height = 0;
+    for (let i = 0; i != animtextureStore.frames.length; i++) {
+      const img = animtextureStore.frames[i].img;
+      console.log(img.naturalWidth, img.naturalHeight);
+      if (img.naturalWidth > maxWidth) maxWidth = img.naturalWidth;
+      height += img.naturalHeight;
+    }
+
+    canvas.width = maxWidth;
+    canvas.height = height;
+
+    ctx.imageSmoothingEnabled = false;
+    for (let i = 0; i != animtextureStore.frames.length; i++) {
+      const img = animtextureStore.frames[i].img;
+      ctx.drawImage(img, 0, i * img.naturalHeight);
+    }
+    const b64 = canvas.toDataURL();
+    const pngd = document.getElementById('pngd') as HTMLAnchorElement;
+    const mcmeta = document.getElementById('mcmeta') as HTMLAnchorElement;
+    pngd.href = b64;
+    pngd.download = animtextureStore.textureName + '.png';
+    mcmeta.download = animtextureStore.textureName + '.png.mcmeta';
+
+    const mcmetajson = {
+      animation: {
+        frames: animtextureStore.frames.map((frame, i) => ({
+          index: i,
+          time: frame.delay,
+        })),
+      },
+    };
+    mcmeta.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(mcmetajson, null, 2));
   });
 
   return (
     <section class="flex mx-auto max-w-6xl px-6 justify-center min-h-svh pt-[72px]">
-      <div class="my-5 min-h-[60px] w-full">
-        <h1 class="font-bold text-gray-50 text-2xl md:text-3xl xl:text-4xl">
-          {t('nav.resources.animatedTextures.title@@Animated Textures')}
-        </h1>
-        <h2 class="text-gray-400 mt-1 mb-5">
-          {t('nav.resources.animatedTextures.description@@Easily merge textures for resource pack animations')}
-        </h2>
+      <div class="flex my-5 min-h-[60px] w-full gap-4">
+        <div class="flex-1">
+          <h1 class="font-bold text-gray-50 text-2xl md:text-3xl xl:text-4xl">
+            {t('nav.resources.animatedTextures.title@@Animated Textures')}
+          </h1>
+          <h2 class="text-gray-400 mt-1 mb-5">
+            {t('nav.resources.animatedTextures.description@@Easily merge textures for resource pack animations')}
+          </h2>
 
-        <div class="flex flex-col gap-2">
-          <label for="fileInput">Select Frame(s) or a GIF</label>
-          <input id="fileInput" type="file" multiple accept="image/*" class="file:lum-btn hover:file:lum-bg-gray-700" onChange$={(e, el) => {
-            const { files } = el;
-            if (!files) return;
-            Array.from(files).forEach(file => {
-              const f = new FileReader();
-              f.readAsDataURL(file);
-              f.onloadend = async (e) => {
-                const b64 = e.target?.result;
-                if (!b64) return;
-                const type = b64.toString().split(',')[0].split(';')[0].split(':')[1];
-                if (type == 'image/gif') {
-                  // @ts-ignore
-                  const gifframes = await gifFrames({ url: b64, frames: 'all', cumulative: animtextureStore.cumulative });
-                  gifframes.forEach((frame: any) => {
-                    const contentStream = frame.getImage();
-                    const imageData = window.btoa(String.fromCharCode.apply(null, contentStream._obj));
-                    const b64frame = `data:image/png;base64,${imageData}`;
+          <div class="grid grid-cols-2 gap-2 mb-4">
+            <div class="flex flex-col">
+              <label for="fileInput">Select Frame(s) or a GIF</label>
+              <input id="fileInput" type="file" multiple accept="image/*" class="file:lum-btn hover:file:lum-bg-gray-700" onChange$={(e, el) => {
+                const { files } = el;
+                if (!files) return;
+                Array.from(files).forEach(file => {
+                  const f = new FileReader();
+                  f.readAsDataURL(file);
+                  f.onloadend = async (e) => {
+                    if (!e.target?.result) return;
 
-                    animtextureStore.frames.push({ img: b64frame, delay: Math.ceil(20 * frame.frameInfo.delay / 100) });
-                  });
-                  return;
-                }
-                animtextureStore.frames.push({ img: b64, delay: 20 });
-              };
-            });
-          }} />
-          <Toggle id="Cumulative" checked={animtextureStore.cumulative}
-            onChange$={(e, el) => { animtextureStore.cumulative = el.checked; }}
-            label={t('animtexture.cumulative@@Cumulative (Turn this on if gif frames are broken)')} />
+                    const frames = [...animtextureStore.frames];
 
-          <label for="textureName">{t('animtexture.textureName@@Texture Name')}</label>
-          <input id="textureName" class={{ 'lum-input': true }} value={animtextureStore.textureName} onInput$={(e, el) => { animtextureStore.textureName = el.value; }}/>
+                    const file = await base64ToFile(e.target.result.toString());
+                    if (file.mime == 'image/gif') {
+                      const parsedGif = parseGIF(file.buffer);
+                      const gifFrames = decompressFrames(parsedGif, true);
+                      gifFrames.forEach((frame) => {
+                        const canvas = document.createElement('canvas');
+                        canvas.width = frame.dims.width;
+                        canvas.height = frame.dims.height;
+                        const ctx = canvas.getContext('2d')!;
+                        const frameImageData = ctx.createImageData(frame.dims.width, frame.dims.height);
+                        frameImageData.data.set(frame.patch);
+                        ctx.putImageData(frameImageData, 0, 0);
+                        const img = new Image();
+                        img.src = canvas.toDataURL();
+                        img.onload = () => {
+                          frames.push({
+                            img,
+                            delay: Math.ceil(20 * frame.delay / 100),
+                          });
+                        };
+                      });
+                    }
+                    else {
+                      const img = new Image();
+                      img.src = e.target.result as string;
+                      frames.push({
+                        img,
+                        delay: 20,
+                      });
+                    }
 
-          <p class="text-gray-50">
-            {t('animtexture.framePreview@@Frame Preview')}
-          </p>
-          <div id="imgs" class="lum-card flex-wrap max-h-[620px] overflow-auto gap-2">
+                    animtextureStore.frames = frames;
+                  };
+                });
+              }} />
+            </div>
+            <div class="flex flex-col">
+              <label for="textureName">{t('animtexture.textureName@@Texture Name')}</label>
+              <input id="textureName" class={{ 'lum-input': true }} value={animtextureStore.textureName} onInput$={(e, el) => { animtextureStore.textureName = el.value; }}/>
+            </div>
+          </div>
+
+          <div id="imgs" class="lum-card flex-row flex-wrap max-h-[620px] overflow-auto gap-2">
             {animtextureStore.frames.map((frame, i) => (
               <div key={`frame${i}`} class="lum-card w-24 p-0">
-                <img width={96} height={96} class="rounded-t-md" src={frame.img} />
+                <img width={96} height={96} class="rounded-t-md" src={frame.img.src} />
                 <input type="number" value={frame.delay}
-                  onInput$={(e, el) => { animtextureStore.frames[i].delay = el.value; }}
+                  onInput$={(e, el) => {
+                    animtextureStore.frames[i].delay = Number(el.value);
+                  }}
                   class="lum-input mb-2 mx-2" />
               </div>
             ))}
           </div>
 
-          <button class={{ 'lum-btn my-6': true }} onClick$={() => {
-            const canvas = document.getElementById('c') as HTMLCanvasElement;
-            canvas.classList.add('sm:flex');
-            const imglist = document.getElementById('imgs') as HTMLDivElement;
-            const ctx = canvas.getContext('2d')!;
-            const imgs = imglist.getElementsByTagName('IMG') as HTMLCollectionOf<HTMLImageElement>;
-            let max = 0;
-            for (let i = 0; i != imgs.length; i++) {
-              if (imgs[i].naturalWidth > max) max = imgs[i].naturalWidth;
-            }
-            canvas.width = max;
-            canvas.height = max * imgs.length;
-            ctx.imageSmoothingEnabled = false;
-            for (let i = 0; i != imgs.length; i++) {
-              ctx.drawImage(imgs[i], 0, i * max);
-              ctx.drawImage(imgs[i], 0, max * i, max, max);
-            }
-            const b64 = canvas.toDataURL();
-            const pngd = document.getElementById('pngd') as HTMLAnchorElement;
-            const mcmeta = document.getElementById('mcmeta') as HTMLAnchorElement;
-            pngd.href = b64;
-            pngd.download = animtextureStore.textureName + '.png';
-            mcmeta.download = animtextureStore.textureName + '.png.mcmeta';
-
-            const start = '{"animation":{"frames": [';
-            const frameBase = '{"index": ';
-            const frameMid = ', "time": ';
-            const frameEnd = '},';
-            let res = start;
-            for (let i = 0; i != animtextureStore.frames.length; i++) {
-              let tmp = frameBase;
-              tmp += i;
-              tmp += frameMid;
-              tmp += animtextureStore.frames[i].delay;
-              tmp += frameEnd;
-              res += tmp;
-            }
-            res = res.substring(0, res.length - 1);
-            res += ']}}';
-
-            mcmeta.href = 'data:text/plain;charset=utf-8,' + res;
-
-            const links = document.getElementById('links')!;
-            links.className = 'inline';
-          }}>
-            {t('animtexture.generate@@Generate')}
-          </button>
-
-        </div>
-
-        <div id="links" class="hidden">
-          <p class="mb-4">Animated Texture Generated Successfully!</p>
-          <div class="flex gap-2">
+          <div id="links" class="flex gap-2 mt-6">
             <a class="lum-btn" id="pngd" href='/'>
+              <Download size={20} />
               {t('animtexture.downloadPNG@@Download PNG')}
             </a>
             <a class="lum-btn" id="mcmeta" target="_blank" href='data:text/plain;charset=utf-8,{"animation":{}}'>
+              <Download size={20} />
               {t('animtexture.downloadMCMETA@@Download MCMETA')}
             </a>
           </div>
         </div>
-
+        <div class="flex flex-col items-center max-w-24">
+          <p class="mb-2">
+            PNG Preview
+          </p>
+          <canvas id="c" class="lum-card max-w-full p-0" />
+        </div>
       </div>
-      <canvas id="c" class="w-24 max-h-svh ml-48 hidden"></canvas>
     </section>
   );
 });
