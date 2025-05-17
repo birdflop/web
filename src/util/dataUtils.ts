e41ae161446efb85dc98ec9c9c93ace143d2de81
saboooor <saboor547@gmail.com>
2025-05-17 09:24:22 -0600

    i cba to support the old cookie version i think it's been about over a year now

--- src/util/dataUtils.ts
+++ src/util/dataUtils.ts
@@ -29,43 +29,13 @@ export function getCookies(cookie: Cookie, name: names, urlParams?: URLSearchPar
     json = { ...json, ...params };
   }
 
-  // Migrate from individual cookies to a single cookie
-  const migrated = migrateCookies(cookie, name, json);
-  if (migrated) cookie.set(name, JSON.stringify(json), { path: '/' });
-
-  return json;
-}
-
-export function migrateCookies(cookie: Cookie, name: names, json: { [key: string]: any }) {
-  if (name != 'rgb' && name != 'animtab') return;
-  const oldNames = name == 'rgb' ? Object.keys(rgbDefaults) : Object.keys(animTABDefaults);
-  if (name == 'animtab') oldNames.push('version');
-  let migrated = false;
-  oldNames.forEach(name => {
-    const cookieValue = cookie.get(name)?.value;
-    if (!cookieValue) return;
-    console.log('Migrating', name);
-    try {
-      if (name == 'colors' || name == 'shadowcolors') json[name] = cookieValue.split(',');
-      else if (name == 'format') json[name] = JSON.parse(cookieValue);
-      else if (cookieValue === 'true' || cookieValue === 'false') json[name] = cookieValue === 'true';
-      else if (!isNaN(Number(cookieValue))) json[name] = Number(cookieValue);
-      else json[name] = cookieValue;
-      migrated = true;
-    }
-    catch (e) {
-      console.error(e);
-    }
-    console.log('Deleting', name);
-    cookie.delete(name, { path: '/' });
-  });
-
   // Migrate between versions
   if (json.version != defaults.version) {
     json = loadPreset(JSON.stringify(json));
-    migrated = true;
+    cookie.set(name, JSON.stringify(json), { path: '/' });
   }
-  return migrated;
+
+  return json;
 }
 
 export function setCookies(name: names, json: { [key: string]: any }) {
