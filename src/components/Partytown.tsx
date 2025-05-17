e12ce43f09917b4571e62eb74557fde3e880bc21
saboooor <saboor547@gmail.com>
2025-05-17 08:33:25 -0600

    oops

--- src/components/Partytown.tsx
+++ src/components/Partytown.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { type PartytownConfig, partytownSnippet } from '@qwik.dev/integration';
+import { type PartytownConfig, partytownSnippet } from '@qwik.dev/partytown/integration';
 
 /**
  * Props for `<QwikPartytown/>`, which extends the Partytown Config.
