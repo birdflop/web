043957411666b12b6d2f65d41df61e33fba846d6
saboooor <saboor547@gmail.com>
2025-05-23 22:32:09 -0600

    uh fix i think

--- src/routes/docs/rgbirdflop/api/layout.tsx
+++ src/routes/docs/rgbirdflop/api/layout.tsx
@@ -1,54 +1,51 @@
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { getGlobalHighlighter } from '~/util/highlighter';
+import { apiEndpoints } from '~/routes/api/v2';
 
 export const useEndpoints = routeLoader$(async ({ url }) => {
-  const data = await fetch(url.origin + '/api/v2');
-  const json = await data.json() as any;
-  const paths = Object.keys(json.endpoints);
+  const paths = Object.keys(apiEndpoints.endpoints) as (keyof typeof apiEndpoints.endpoints)[];
+  const json: typeof apiEndpoints = JSON.parse(JSON.stringify(apiEndpoints));
 
   for (const path of paths) {
     const endpointData = await fetch(url.origin + path);
-    const endpointJson = await endpointData.json() as any;
-    json.endpoints[path] = { methods: json.endpoints[path], options: endpointJson.options };
+    const { options } = await endpointData.json() as any;
+
+    const highlighter = await getGlobalHighlighter();
+
+    const html = Object.keys(options).map(option => (
+      highlighter.codeToHtml(`// ${options[option].description}
+${option}: ${options[option].type} = ${JSON.stringify(options[option].default, null, 2)}`, {
+        lang: 'ts',
+        theme: 'birdflop',
+        meta: {
+          title: option,
+          description: options[option].description,
+        },
+      })
+    ));
+
+    json.endpoints[path].html = html;
   }
-
-  return json;
-});
-
-export const useHighlighter = routeLoader$(async () => {
-  return await getGlobalHighlighter();
+  return json.endpoints;
 });
 
 export const Endpoints = component$(() => {
-  const { endpoints } = useEndpoints().value;
-  const highlighter = useHighlighter().value;
+  const endpoints = useEndpoints().value;
+  const endpointNames = Object.keys(endpoints) as (keyof typeof endpoints)[];
 
-  return Object.keys(endpoints).map((path) => <>
+  return endpointNames.map((path) => <>
     <h3>
       {path}
     </h3>
     {Object.entries(endpoints[path].methods).map(([method, description]) =>
       <p key={method}>
-        {method}: {description as string}
+        {method}: {description}
       </p>,
     )}
     <h4>
       Options
     </h4>
-    {Object.keys(endpoints[path].options).map(option => {
-      const html = highlighter.codeToHtml(`// ${endpoints[path].options[option].description}
-${option}: ${endpoints[path].options[option].type} = ${JSON.stringify(endpoints[path].options[option].default, null, 2)}`, {
-        lang: 'ts',
-        theme: 'birdflop',
-        meta: {
-          title: option,
-          description: endpoints[path].options[option].description,
-        },
-      });
-      return <>
-        <div dangerouslySetInnerHTML={html} />
-      </>;
-    })}
+    {endpoints[path].html?.map((option, i) => <div key={i} dangerouslySetInnerHTML={option} />)}
   </>);
 });
