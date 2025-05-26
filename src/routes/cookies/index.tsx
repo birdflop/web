88557774d0a3b81355265e78f9e3e54c815b13e7
saboooor <saboor547@gmail.com>
2025-05-25 18:12:10 -0600

    format so that ez

--- src/routes/cookies/index.tsx
+++ src/routes/cookies/index.tsx
@@ -1,11 +1,28 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 
-export const onGet: RequestHandler = ({ json, cookie }) => {
+export const onGet: RequestHandler = ({ text, json, cookie, query }) => {
   // get all cookies
   const cookies = cookie.getAll();
   console.debug('cookies', cookies);
   Object.keys(cookies).forEach((key) => {
-    if (key.startsWith('authjs')) delete cookies[key];
+    if (key.includes('authjs') || key.includes('FCNEC')) delete cookies[key];
   });
-  throw json(200, cookies);
+  const cookiesObject = Object.fromEntries(
+    Object.entries(cookies).map(([key, value]) => {
+      if (value.value.startsWith('{"')) {
+        try {
+          value.value = JSON.parse(value.value);
+        } catch (e) {
+          console.error(`Failed to parse cookie ${key}:`, e);
+        }
+      }
+      return [key, value.value];
+    }),
+  );
+  console.debug('cookiesObject', cookiesObject);
+
+  if (query.get('text') !== null) {
+    throw text(200, JSON.stringify(cookiesObject, null, 2));
+  }
+  throw json(200, cookiesObject);
 };
