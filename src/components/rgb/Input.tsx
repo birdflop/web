d9b3957c94f32baee8b743816adb64acb0613d52
saboooor <saboor547@gmail.com>
2025-06-12 19:03:11 -0600

    new input

--- src/components/rgb/Input.tsx
+++ src/components/rgb/Input.tsx
@@ -22,20 +22,9 @@ const InputField = component$(({ class: className, readOnly }: {
         'font-mc-italic': rgbStore.italic,
         'font-mc-bold-italic': rgbStore.bold && rgbStore.italic,
         [`${rgbStore.format.class}`]: rgbStore.format.class,
-      }}>
+      }} contentEditable={readOnly ? 'false' : 'true'}>
         <Slot />
       </p>
-      <div class="absolute bottom-0 h-full flex flex-col w-[calc(100%+0.5rem)]">
-        <textarea class={{
-          'lum-input p-0 pt-1.5 -mb-2.5 rounded-sm resize-none w-full h-full whitespace-pre-wrap caret-white leading-none text-transparent hover:text-transparent lum-bg-transparent hover:lum-bg-transparent hover:outline-1 hover:outline-gray-400/50 line': true,
-          'font-mc-bold': rgbStore.bold,
-          'font-mc-italic': rgbStore.italic,
-          'font-mc-bold-italic': rgbStore.bold && rgbStore.italic,
-          [`${className}`]: className,
-          [`${rgbStore.format.class}`]: rgbStore.format.class,
-        }} value={rgbStore.text} spellcheck={false} readOnly={readOnly} id="input"
-        onInput$={(e, el) => { rgbStore.text = el.value; }}/>
-      </div>
     </div>
   );
 });
@@ -136,11 +125,12 @@ export default component$(({ readOnly }: {
         'font-mc-bold-italic': rgbStore.bold && rgbStore.italic,
         [`${rgbStore.format.class}`]: rgbStore.format.class,
       }}>
-        <p class="lum-bg-gray-800/50 rounded-lg lum-btn-p-2 w-full h-full pointer-events-none whitespace-pre-wrap!">
+        <p contentEditable={readOnly ? 'false' : 'true'} class="lum-bg-gray-800/50 rounded-lg lum-btn-p-2 w-full h-full whitespace-pre-wrap!"
+          onInput$={(e, el) => {
+            rgbStore.text = el.textContent || '';
+          }}>
           <Slot />
         </p>
-        <textarea readOnly={readOnly} class="absolute top-0 lum-input lum-btn-p-2 resize-none w-full h-full whitespace-pre-wrap! caret-white text-transparent lum-bg-transparent hover:text-transparent hover:lum-bg-transparent hover:backdrop-brightness-150" id="input"
-          value={rgbStore.text} spellcheck={false} onInput$={(e, el) => { rgbStore.text = el.value; }}/>
       </div>
     }
     <div class={{
