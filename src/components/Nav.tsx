505f8e8a265679bb4738d7f244858da22247f743
saboooor <saboor547@gmail.com>
2025-07-17 04:31:22 -0600

    a

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -39,7 +39,7 @@ export default component$(() => {
       <Link q:slot="end" href="/docs" class="lum-btn lum-bg-transparent hover:lum-bg-nav-bg hidden sm:flex">
         <Book size={20} /> {t('nav.docs@@Docs')}
       </Link>
-      <SelectMenuRaw id="nav-hosting" q:slot='end' hover customDropdown
+      <SelectMenuRaw id="nav-hosting" q:slot='end' hover customDropdown panelClass='lum-bg-nav-bg'
         class={{ 'lum-bg-transparent hover:lum-bg-nav-bg hidden sm:flex': true }}>
         <div q:slot="dropdown" class="flex items-center gap-2">
           <Server size={20} /> {t('nav.hosting.title@@Hosting')}
@@ -57,7 +57,7 @@ export default component$(() => {
           <Activity size={20} /> {t('nav.hosting.nodeStats.title@@Node Stats')}
         </Link>
       </SelectMenuRaw>
-      <SelectMenuRaw id="nav-resources" q:slot='end' hover customDropdown
+      <SelectMenuRaw id="nav-resources" q:slot='end' hover customDropdown panelClass='lum-bg-nav-bg'
         class={{ 'lum-bg-transparent hover:lum-bg-nav-bg hidden sm:flex': true }}>
         <div q:slot="dropdown" class="flex items-center gap-2">
           <Box size={20} /> {t('nav.resources.title@@Resources')}
@@ -81,16 +81,19 @@ export default component$(() => {
           <Ellipsis size={20} /> {t('nav.resources.more@@More Resources')}
         </Link>
       </SelectMenuRaw>
-      <SelectMenuRaw align="right" q:slot='end' class={{ 'hidden': !loc.url.pathname.includes('resources'), 'p-2 lum-bg-transparent hover:lum-bg-nav-bg gap-1': true }} id="lang-picker" customDropdown
-        values={config.supportedLocales.map(value => (
-          {
-            name: languages[value.lang as keyof typeof languages],
-            value: value.lang,
-          }
-        ))} onChange$={(e, el) => {
-          document.cookie = `locale=${JSON.stringify(config.supportedLocales.find(locale => locale.lang == el.value))};max-age=86400;path=/`;
-          location.reload();
-        }}>
+      <SelectMenuRaw align="right" q:slot='end' class={{
+        'hidden': !loc.url.pathname.includes('resources'),
+        'p-2 lum-bg-transparent hover:lum-bg-nav-bg gap-1': true,
+      }} id="lang-picker" customDropdown panelClass='lum-bg-nav-bg'
+      values={config.supportedLocales.map(value => (
+        {
+          name: languages[value.lang as keyof typeof languages],
+          value: value.lang,
+        }
+      ))} onChange$={(e, el) => {
+        document.cookie = `locale=${JSON.stringify(config.supportedLocales.find(locale => locale.lang == el.value))};max-age=86400;path=/`;
+        location.reload();
+      }}>
         <span class="absolute top-0 left-5 text-[10px] lum-bg-nav-bg rounded-sm px-0.5" q:slot='dropdown'>
           {locale.lang.split('-')[0]}
         </span>
@@ -101,7 +104,9 @@ export default component$(() => {
         <SocialButtons />
       </div>
       {session.value && session.value.user &&
-        <SelectMenuRaw align="right" q:slot='end' class={{ 'p-2 lum-bg-transparent hover:lum-bg-nav-bg gap-1': true }} id="profile" customDropdown>
+        <SelectMenuRaw align="right" q:slot='end' class={{
+          'p-2 lum-bg-transparent hover:lum-bg-nav-bg gap-1': true,
+        }} id="profile" customDropdown panelClass='lum-bg-nav-bg'>
           <p q:slot='dropdown' class="flex items-center gap-2 text-lum-text">
             {session.value.user.image &&
               <img src={session.value.user.image} width={20} height={20} class="rounded-full! min-w-5 h-5" />
