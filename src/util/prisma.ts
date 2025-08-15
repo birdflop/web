1ac25d795ea7621101896cc738bb49e31be097d5
saboooor <saboor547@gmail.com>
2025-08-15 05:37:01 -0600

    Revert "fix prisma"
    This reverts commit 9d121d3dc5e1d8fb8a6f0de01dbe11a9dd175bfa.

--- src/util/prisma.ts
+++ src/util/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '~/prisma/edge';
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 function createPrismaClient(url: string) {
