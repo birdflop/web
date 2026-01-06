170f0b818710b03a7f8872f3856fedb71de9b9ce
saboooor <saboor547@gmail.com>
2026-01-06 00:18:49 -0500

    Revert "refactor: simplify InputField structure"
    This reverts commit 4082d28eedb268b47b1da8112eca8637582d5977.

--- src/components/Rgbirdflop/Input.tsx
+++ src/components/Rgbirdflop/Input.tsx
@@ -17,24 +17,33 @@ const InputField = component$(({ class: className, readOnly }: {
 }) => {
   const rgbStore = useContext(rgbStoreContext);
   return (
-    <p
-      contentEditable={readOnly ? 'false' : 'true'}
-      spellcheck={false}
-      class={{
-        'relative text-2xl wrap-break-word outline-none whitespace-pre-wrap caret-white': true,
+    <div class={{
+      'relative text-2xl wrap-break-word': true,
+      [`${className}`]: className,
+    }}
+    style={{ textShadow: '2px 2px 0 #373737' }}>
+      <p class={{
         'font-mc-bold': rgbStore.bold,
         'font-mc-italic': rgbStore.italic,
         'font-mc-bold-italic': rgbStore.bold && rgbStore.italic,
-        [`${className}`]: className,
         [`${rgbStore.format.class}`]: rgbStore.format.class,
-      }}
-      style={{ textShadow: '2px 2px 0 #373737' }}
-      onInput$={(e, el) => {
-        rgbStore.text = el.innerText;
-      }}
-    >
-      <Slot />
-    </p>
+      }}>
+        <Slot />
+      </p>
+      {!readOnly &&
+        <div class="absolute bottom-0 h-full flex flex-col w-[calc(100%+0.5rem)]">
+          <textarea class={{
+            'lum-input p-0 pt-1.5 -mb-2.5 rounded-sm resize-none w-full h-full whitespace-pre-wrap caret-white leading-none text-transparent hover:text-transparent lum-bg-transparent hover:lum-bg-transparent hover:outline-1 hover:outline-gray-400/50 line': true,
+            'font-mc-bold': rgbStore.bold,
+            'font-mc-italic': rgbStore.italic,
+            'font-mc-bold-italic': rgbStore.bold && rgbStore.italic,
+            [`${className}`]: className,
+            [`${rgbStore.format.class}`]: rgbStore.format.class,
+          }} value={rgbStore.text} spellcheck={false} id="input"
+          onInput$={(e, el) => { rgbStore.text = el.value; }}/>
+        </div>
+      }
+    </div>
   );
 });
 
