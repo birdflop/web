b8e992bb4c39ec7b6b574aa47bdb8353dce4272e
saboooor <saboor547@gmail.com>
2026-01-08 16:08:31 -0500

    fix: use updated accordion on animtab

--- src/routes/resources/animtab/index.tsx
+++ src/routes/resources/animtab/index.tsx
@@ -248,7 +248,11 @@ export default component$(() => {
             <Output hidden={!openItemsStore.items.includes('output')}
               value={AnimationOutput(rgbStore, animtabStore)} />
 
-            <Options>
+            <Accordion sectionName='options' alwaysOpen>
+              <Settings size={26} />
+              {t('rgb.options@@Options')}
+            </Accordion>
+            <Options hidden={!openItemsStore.items.includes('options')}>
               <div class="flex flex-col gap-1 col-span-2">
                 <label for="nameinput">
                   {t('animtab.animation.name@@Animation Name')}
