b7450db118f7311390b47b91740ed5be8e1877b5
saboooor <saboor547@gmail.com>
2026-01-08 15:54:57 -0500

    disable guided tour again

--- src/routes/resources/rgb/index.tsx
+++ src/routes/resources/rgb/index.tsx
@@ -287,6 +287,7 @@ export default component$(() => {
 
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(() => {
+    return; // Disable guided tour for now
     const notification = new Notification()
       .setTitle('Flopbird:')
       .setDescription('Hi! I\'m here to help you create RGB gradients!')
