95a3ed46c0fec91ce77b87a96e8587831201ec9e
saboooor <saboor547@gmail.com>
2026-01-08 23:13:10 -0500

    feat: enhance mobile UI with collapsible buttons for color and text shadow options

--- src/routes/resources/rgb/index.tsx
+++ src/routes/resources/rgb/index.tsx
@@ -367,32 +367,67 @@ export default component$(() => {
         </div>
 
         <ColorMap />
-
         <div class='grid sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-2 mt-1'>
-          <div class="lum-card flex-row gap-1 *:lum-btn *:rounded-lum-1 p-1 my-2 max-w-full overflow-auto">
-            <button>
+          <div class="sm:hidden lum-card flex-row gap-1 *:lum-btn *:rounded-lum-1 p-1 my-2 min-w-0 w-full overflow-auto">
+            <button onClick$={() => {
+              openItemsStore.items = openItemsStore.items.includes('colors')
+                ? openItemsStore.items.filter(item => item !== 'colors')
+                : ['colors'];
+            }} class={{
+              'lum-bg-blue!': openItemsStore.items.includes('colors'),
+            }}>
               <Palette />
               {t('rgb.colors.title@@Colors')}
             </button>
-            <button>
+            <button onClick$={() => {
+              openItemsStore.items = openItemsStore.items.includes('textshadow')
+                ? openItemsStore.items.filter(item => item !== 'textshadow')
+                : ['textshadow'];
+            }} class={{
+              'lum-bg-blue!': openItemsStore.items.includes('textshadow'),
+            }}>
               <Blend />
               {t('rgb.colors.shadow.title@@Text Shadow')}
             </button>
-            <button>
+            <button onClick$={() => {
+              openItemsStore.items = openItemsStore.items.includes('output')
+                ? openItemsStore.items.filter(item => item !== 'output')
+                : ['output'];
+            }} class={{
+              'lum-bg-blue!': openItemsStore.items.includes('output'),
+            }}>
               <Clipboard />
               {t('rgb.output.title@@Output')}
             </button>
-            <button>
+            <button onClick$={() => {
+              openItemsStore.items = openItemsStore.items.includes('options')
+                ? openItemsStore.items.filter(item => item !== 'options')
+                : ['options'];
+            }} class={{
+              'lum-bg-blue!': openItemsStore.items.includes('options'),
+            }}>
               <Settings />
               {t('rgb.options@@Options')}
             </button>
             {rgbStore.customFormat && (
-              <button>
+              <button onClick$={() => {
+                openItemsStore.items = openItemsStore.items.includes('formatoptions')
+                  ? openItemsStore.items.filter(item => item !== 'formatoptions')
+                  : ['formatoptions'];
+              }} class={{
+                'lum-bg-blue!': openItemsStore.items.includes('formatoptions'),
+              }}>
                 <Settings />
                 {t('rgb.formatting.options@@Format Options')}
               </button>
             )}
-            <button>
+            <button onClick$={() => {
+              openItemsStore.items = openItemsStore.items.includes('decode')
+                ? openItemsStore.items.filter(item => item !== 'decode')
+                : ['decode'];
+            }} class={{
+              'lum-bg-blue!': openItemsStore.items.includes('decode'),
+            }}>
               <Sparkles />
               {t('rgb.decode.title@@Decode')}
               <span class='lum-bg-blue/50 text-xs py-1 px-2 rounded-lum-1'>
