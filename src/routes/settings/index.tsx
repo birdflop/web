ebf028a5fef94f8fa635d917a5ea775ed49e1e57
saboooor <saboor547@gmail.com>
2026-01-23 03:29:59 -0500

    refactor: update settings page layout

--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -12,38 +12,36 @@ export default component$(() => {
   const locale = useSpeakLocale();
 
   return (
-    <section class="flex mx-auto max-w-xl px-6 justify-center min-h-svh pt-20">
-      <div class="min-h-15 w-full">
-        <h1 class='flex gap-3 text-2xl! items-center my-2!'>
-          <Settings size={32} />
-          {t('nav.settings.title@@Settings')}
-        </h1>
-        <p class="mb-4 border-b border-lum-border/10 pb-4">
-          {t('nav.settings.description@@Manage your settings and preferences here.')}
-        </p>
+    <section class="flex flex-col mx-auto max-w-6xl px-6 min-h-svh pt-20">
+      <h1 class='flex gap-3 text-2xl! items-center my-2!'>
+        <Settings size={32} />
+        {t('nav.settings.title@@Settings')}
+      </h1>
+      <p class="mb-4 border-b border-lum-border/10 pb-4">
+        {t('nav.settings.description@@Manage your settings and preferences here.')}
+      </p>
 
-        <div class="flex flex-col gap-2">
-          <SelectMenu id="lang-picker" panelClass='lum-bg-nav-bg'
-            values={config.supportedLocales.map(value => (
-              {
-                name: languages[value.lang as keyof typeof languages],
-                value: value.lang,
-              }
-            ))}
-            value={locale.lang}
-            onChange$={(e, el) => {
-              document.cookie = `locale=${JSON.stringify(config.supportedLocales.find(locale => locale.lang == el.value))};max-age=86400;path=/`;
-              location.reload();
-            }}>
-            {t('settings.language@@Language')}
-          </SelectMenu>
+      <div class="flex flex-col gap-2">
+        <SelectMenu id="lang-picker" panelClass='lum-bg-nav-bg'
+          values={config.supportedLocales.map(value => (
+            {
+              name: languages[value.lang as keyof typeof languages],
+              value: value.lang,
+            }
+          ))}
+          value={locale.lang}
+          onChange$={(e, el) => {
+            document.cookie = `locale=${JSON.stringify(config.supportedLocales.find(locale => locale.lang == el.value))};max-age=86400;path=/`;
+            location.reload();
+          }}>
+          {t('settings.language@@Language')}
+        </SelectMenu>
 
-          <div class="flex flex-col gap-1">
-            <label for="theme">
-              {t('settings.theme@@Theme Preference')}
-            </label>
-            <ThemeToggle variant="full" showLabel />
-          </div>
+        <div class="flex flex-col gap-1">
+          <label for="theme">
+            {t('settings.theme@@Theme Preference')}
+          </label>
+          <ThemeToggle variant="full" showLabel />
         </div>
       </div>
     </section>
@@ -53,5 +51,4 @@ export default component$(() => {
 export const head = generateHead({
   title: 'Settings - Birdflop',
   description: 'Manage your Birdflop settings and preferences. ' + defaultDescription,
-});
 });
