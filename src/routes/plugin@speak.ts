17e511a4240f3a5fcb5c918bf715d15a041779b9
saboooor <saboor547@gmail.com>
2026-01-27 19:38:28 -0500

    refactor: streamline language detection logic in onRequest handler

--- src/routes/plugin@speak.ts
+++ src/routes/plugin@speak.ts
@@ -1,23 +1,23 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { getCookies } from '~/util/dataUtils';
-// import { config } from '../speak-config';
+import { languages } from '~/speak-config';
 
 export const onRequest: RequestHandler = ({ request, locale, cookie }) => {
   const acceptLanguage = request.headers?.get('accept-language');
 
-  let lang: string | null = null;
+  let lang: keyof typeof languages | undefined = undefined;
 
   // Try whether the language is stored in a cookie
-  if (cookie) {
-    const { cookies: settings } = getCookies(cookie, 'settings');
-    if (settings) lang = settings.locale;
-  }
+  const { cookies: settings } = getCookies(cookie, 'settings');
 
-  // Try to use user language
-  if (!lang && acceptLanguage) {
-    lang = acceptLanguage.split(';')[0]?.split(',')[0];
-  }
+  // Check for old locale cookie as well
+  const oldLocale = cookie.get('locale')?.json() as {
+    lang: keyof typeof languages
+  };
 
-  // Set Qwik locale
-  locale(lang || undefined);
+  if (settings.locale) lang = settings.locale;
+  else if (oldLocale) lang = oldLocale.lang;
+
+  // Set Qwik locale based on cookie or Accept-Language header
+  locale(lang || acceptLanguage?.split(';')[0]?.split(',')[0]);
 };
