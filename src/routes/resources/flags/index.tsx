18edcee1a2158fab4e53dbd74561730bb23aac5f
saboooor <saboor547@gmail.com>
2026-03-09 02:46:46 -0300

    extract keys

--- src/routes/resources/flags/index.tsx
+++ src/routes/resources/flags/index.tsx
@@ -214,7 +214,7 @@ export default component$(() => {
                 flagsStore.calcOverhead = el.checked;
               }}>
                 <MemoryStick />
-                {t('flags.memory.calcOverhead@@Calculate Overhead')}
+                {t('flags.memory.calcOverhead.title@@Calculate Overhead')}
               </Toggle>
               <p class="text-sm whitespace-pre-wrap">
                 {t('flags.memory.calcOverhead.description@@This is recommended to avoid out-of-memory issues on your server.\nThe formula used is 11x ÷ 12 - 1200 where x is the amount of RAM.')}
