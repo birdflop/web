79f884e3994346413c58320146fc377ac530662f
saboooor <saboor547@gmail.com>
2026-04-05 17:25:12 -0300

    uhh

--- src/components/analyze/Redirect.tsx
+++
@@ -1,7 +0,0 @@
-import { RequestHandler } from '@builder.io/qwik-city';
-
-// redirect to analyze page.
-export const onGet: RequestHandler = ({ redirect, params }) => {
-  if (params.id) redirect(301, `/resources/analyze/${params.id}`);
-  else redirect(301, '/resources/analyze');
-};
--- src/routes/resources/papertimings/index.tsx
+++ src/routes/resources/papertimings/index.tsx
@@ -1,1 +1,7 @@
-export { onGet } from '~/components/analyze/Redirect';
+import { RequestHandler } from '@builder.io/qwik-city';
+
+// redirect to analyze page.
+export const onGet: RequestHandler = ({ redirect, params }) => {
+  if (params.id) redirect(301, `/resources/analyze/${params.id}`);
+  else redirect(301, '/resources/analyze');
+};
