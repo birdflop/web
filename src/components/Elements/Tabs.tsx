7e34b8d8d6ea059551b539fd4ec8517438fe4183
saboooor <github@saboor.ca>
2026-07-08 16:23:24 -0400

    fix: update TabsProps to use correct PropFunction type for onPlus$ and clean up code

--- src/components/Elements/Tabs.tsx
+++ src/components/Elements/Tabs.tsx
@@ -1,9 +1,9 @@
-import { component$, PropFunction } from '@builder.io/qwik';
+import { component$, PropFunction, PropsOf } from '@builder.io/qwik';
 import { ButtonContainer, ButtonContainerProps } from './ButtonContainer';
 import { Plus, X } from 'lucide-icons-qwik';
 
 interface TabsProps extends Omit<ButtonContainerProps, 'onClick$'> {
-  onPlus$?: PropFunction<() => void>;
+  onPlus$?: PropFunction<PropsOf<'button'>['onClick$']>;
   onClick$?: PropFunction<(value: string) => void>;
   onDelete$?: PropFunction<(value: string) => void>;
   values?: string[];
@@ -11,8 +11,6 @@ interface TabsProps extends Omit<ButtonContainerProps, 'onClick$'> {
 }
 
 export const Tabs = component$<TabsProps>(({ class: classList, onPlus$, values, value, onClick$, onDelete$, ...props }) => {
-  console.log(value);
-
   return (
     <ButtonContainer { ...props }
       class={{
@@ -24,10 +22,13 @@ export const Tabs = component$<TabsProps>(({ class: classList, onPlus$, values, 
           'p-0!': true,
           'lum-grad-bg-lum-accent!': value == valueString,
         }}>
-          <button class="lum-btn-p-1 pr-0" onClick$={() => onClick$?.(valueString)}>
+          <button class="lum-btn-p-1 pr-0"
+            onClick$={() => onClick$?.(valueString)}>
             {valueString}
           </button>
-          <button class="lum-btn lum-bg-transparent hover:lum-bg-red p-0.5 rounded-lum-2 m-1" onClick$={() => onDelete$?.(valueString)}>
+          <button class="lum-btn lum-bg-transparent hover:lum-bg-red p-0.5 rounded-lum-2 m-1"
+            title={`Delete ${valueString}`}
+            onClick$={() => onDelete$?.(valueString)}>
             <X size={16} />
           </button>
         </div>)
